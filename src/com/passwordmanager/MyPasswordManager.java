c3d6891c9eff629491755deb187fb32d91ebcd05
Sébastien Duché <sebastienduche@hotmail.com>
2021-07-08 14:56:37 +0200

    Add version file generator

--- src/com/passwordmanager/MyPasswordManager.java
+++ src/com/passwordmanager/MyPasswordManager.java
@@ -45,7 +45,8 @@ import java.util.prefs.Preferences;
 
 public final class MyPasswordManager extends JFrame {
 
-  public static final String VERSION = "1.0";
+	public static final String INTERNAL_VERSION = "1.0";
+  public static final String VERSION = "1";
 
   private final JMenuItem newFile = new JMenuItem(new NewFileAction());
   private final JMenuItem openFile = new JMenuItem(new OpenFileAction());
